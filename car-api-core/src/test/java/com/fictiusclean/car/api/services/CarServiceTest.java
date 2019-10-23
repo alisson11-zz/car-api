a6a7063d4a1594d0ae05b5a4b8a879779bc50438
alisson11 <alisson.maia11@gmail.com>
2019-10-23 15:33:24 -0300

    added unit tests for carService.retrieveTotalExpenses

--- car-api-core/src/test/java/com/fictiusclean/car/api/services/CarServiceTest.java
+++ car-api-core/src/test/java/com/fictiusclean/car/api/services/CarServiceTest.java
@@ -1,13 +1,20 @@
 package com.fictiusclean.car.api.services;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.comparesEqualTo;
+import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.Month;
 import java.time.ZoneId;
+import java.util.Collections;
 import java.util.Date;
+import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,6 +23,8 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import com.fictiusclean.car.api.dtos.CarDTO;
+import com.fictiusclean.car.api.dtos.CarDetailsRequestDTO;
+import com.fictiusclean.car.api.dtos.CarDetailsResponseDTO;
 import com.fictiusclean.car.api.entities.Car;
 import com.fictiusclean.car.api.mappers.CarMapper;
 import com.fictiusclean.car.api.repositories.CarRepository;
@@ -54,6 +63,26 @@ public class CarServiceTest {
 		verify( carRepository ).save( carEntity );
 	}
 
+	@Test
+	public void whenRetrieveConsumptionDetails() {
+		CarDetailsRequestDTO detailsRequest = buildRequest();
+		when( carRepository.findAll() ).thenReturn( Collections.singletonList( buildCar() ) );
+
+		List<CarDetailsResponseDTO> resultList = carService.retrieveTotalExpenses( detailsRequest );
+
+		verifyZeroInteractions( mapper );
+		verify( validationService ).validate( detailsRequest );
+		verify( carRepository ).findAll();
+		assertThat( resultList, hasSize( 1 ) );
+		CarDetailsResponseDTO responseDTO = resultList.get( 0 );
+		assertThat( responseDTO.getName(), equalTo( NAME ) );
+		assertThat( responseDTO.getBrand(), equalTo( BRAND ) );
+		assertThat( responseDTO.getModel(), equalTo( MODEL ) );
+		assertThat( responseDTO.getYear(), equalTo( 1996 ) );
+		assertThat( responseDTO.getSpentFuelTotalQuantity(), comparesEqualTo( BigDecimal.valueOf( 24.464D ) ) );
+		assertThat( responseDTO.getSpentFuelTotalValue(), comparesEqualTo( BigDecimal.valueOf( 97.122D ) ) );
+	}
+
 	private CarDTO buildCarDTO() {
 		return CarDTO.builder()
 				.model( MODEL )
@@ -73,6 +102,14 @@ public class CarServiceTest {
 				.manufacturingDate( MANUFACTURING_DATE_AS_DATE )
 				.averageCityConsumption( AVERAGE_CITY_CONSUMPTION )
 				.averageHighwayConsumption( AVERAGE_HIGHWAY_CONSUMPTION )
+				.build();
+	}
+
+	private CarDetailsRequestDTO buildRequest() {
+		return CarDetailsRequestDTO.builder()
+				.fuelPrice( BigDecimal.valueOf( 3.97D ) )
+				.cityRouteKilometers( BigDecimal.valueOf( 100D ) )
+				.highwayRouteKilometers( BigDecimal.valueOf( 200D ) )
 				.build();
 	}
 }
