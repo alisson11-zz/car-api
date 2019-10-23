4403875ffc3792c2fef23dfcf8c17a8fdf1934cd
alisson11 <alisson.maia11@gmail.com>
2019-10-23 16:20:09 -0300

    using expenses calculator service on carService

--- car-api-core/src/main/java/com/fictiusclean/car/api/services/CarService.java
+++ car-api-core/src/main/java/com/fictiusclean/car/api/services/CarService.java
@@ -1,11 +1,5 @@
 package com.fictiusclean.car.api.services;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.time.LocalDate;
-import java.time.ZoneId;
-import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
 import lombok.AllArgsConstructor;
@@ -27,6 +21,7 @@ public class CarService {
 	private CarRepository carRepository;
 	private CarMapper mapper;
 	private ValidationService validationService;
+	private ExpensesCalculatorService expensesCalculatorService;
 
 	@Transactional
 	public CarDTO save(CarDTO carDTO) {
@@ -37,44 +32,6 @@ public class CarService {
 
 	public List<CarDetailsResponseDTO> retrieveTotalExpenses(CarDetailsRequestDTO request) {
 		validationService.validate( request );
-		List<Car> carList = carRepository.findAll();
-		List<CarDetailsResponseDTO> responseDTOList = new ArrayList();
-		carList.forEach( car -> calculate( car, request, responseDTOList ) );
-		responseDTOList.sort( Comparator.comparing( CarDetailsResponseDTO::getSpentFuelTotalValue ) );
-		return responseDTOList;
-	}
-
-	private void calculate(Car car, CarDetailsRequestDTO request, List<CarDetailsResponseDTO> responseDTOList) {
-		BigDecimal cityLiters = calculate( request.getCityRouteKilometers(), car.getAverageCityConsumption() );
-		BigDecimal highwayLiters = calculate( request.getHighwayRouteKilometers(), car.getAverageHighwayConsumption() );
-		addToList( car, request, responseDTOList, sum( cityLiters, highwayLiters ) );
-	}
-
-	private void addToList(Car car, CarDetailsRequestDTO request, List<CarDetailsResponseDTO> responseDTOList,
-			BigDecimal totalSpentFuel) {
-		responseDTOList.add( CarDetailsResponseDTO.builder()
-				.name( car.getName() )
-				.brand( car.getBrand() )
-				.model( car.getModel() )
-				.year( LocalDate.ofInstant( car.getManufacturingDate().toInstant(), ZoneId.systemDefault() ).getYear() )
-				.spentFuelTotalQuantity( totalSpentFuel )
-				.spentFuelTotalValue( multiply( totalSpentFuel, request.getFuelPrice() ) )
-				.build() );
-	}
-
-	private BigDecimal calculate(BigDecimal totalRouteKilometers, BigDecimal totalConsumption) {
-		return totalRouteKilometers.divide( totalConsumption, 3, RoundingMode.HALF_EVEN );
-	}
-
-	private BigDecimal sum(BigDecimal firstValue, BigDecimal secondValue) {
-		return firstValue.add( secondValue );
-	}
-
-	private BigDecimal multiply(BigDecimal value, BigDecimal multiplyFactor) {
-		return round( value.multiply( multiplyFactor ) );
-	}
-
-	private BigDecimal round(BigDecimal value) {
-		return value.setScale( 3, RoundingMode.HALF_EVEN );
+		return expensesCalculatorService.retrieveDetails( carRepository.findAll(), request );
 	}
 }
